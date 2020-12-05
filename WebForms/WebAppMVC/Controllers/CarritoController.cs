66272fdae5b51ba6599bc69d1512fadfb93531bd
Willy <willy@correomodificado.com>
2020-12-05 17:55:32 -0500

    Listado

--- WebForms/WebAppMVC/Controllers/CarritoController.cs
+++ WebForms/WebAppMVC/Controllers/CarritoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SisPlaBLL;
 
 namespace WebAppMVC.Controllers
 {
@@ -11,7 +12,7 @@ namespace WebAppMVC.Controllers
         // GET: Carrito
         public ActionResult Index()
         {
-            return View();
+            return View(SisplaLogic.PortadaListar());
         }
     }
 }
